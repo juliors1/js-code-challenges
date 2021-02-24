7e3a40d49bba1073f77090f6dd52059c48778570
Juliors1 <juliorojassolis@gmail.com>
2021-02-24 12:47:38 -0500

    complete challenge 2

--- challanges.js
+++ challanges.js
@@ -32,7 +32,53 @@ const twoSum = function(nums, target) {
     }
     return result;
 };
-console.log(twoSum([3,2,4], 6))
+// console.log(twoSum([3,2,4], 6))
 
 
   /*-----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+Challenge: 2 - Add Two Numbers
+Difficulty: Medium
+Prompt:
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+-----------------------------------------------------------------------------*/
+// Your solution for 2 - Add Two Numbers here:
+var addTwoNumbers = function(l1, l2) {
+    var List = new ListNode(0);
+    var head = List;
+    var sum = 0;
+    var carry = 0;
+
+    while(l1!==null||l2!==null||sum>0){
+
+        if(l1!==null){
+            sum = sum + l1.val;
+            l1 = l1.next;
+        }
+        if(l2!==null){
+            sum = sum + l2.val;
+            l2 = l2.next;
+        }
+        if(sum>=10){
+            carry = 1;
+            sum = sum - 10;
+        }
+
+        head.next = new ListNode(sum);
+        head = head.next;
+
+        sum = carry;
+        carry = 0;
+
+    }
+
+    return List.next;
+};
